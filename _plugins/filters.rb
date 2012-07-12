7229e944e0951f3f4e291f7bb2dc00a34eebd6e8
vzvu3k6k <vzvu3k6k@gmail.com>
2012-07-12 22:15:52 +0900

    記事を作った日付をtime要素で表示

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -4,5 +4,11 @@ module Jekyll
     def date_to_ja_string(date)
       date.strftime("%Y年%m月%d日")
     end
+
+    # Format a date to html5 valid date string
+    # http://www.w3.org/TR/html5/common-microsyntaxes.html#valid-date-string
+    def date_to_valid_date_string(date)
+      date.strftime("%Y-%m-%d")
+    end
   end
 end
