d4561af71d6d97eedf6c832cb2f48705c5ca7bac
vzvu3k6k <vzvu3k6k@gmail.com>
2013-11-19 23:57:07 +0900

    Implement Spider (rack middleware)
    Spider detects URLs of resources referenced in a requested pages
    and calls Sitespec.get for each of them.
    
    But its URL detection is incomplete and I think it is very
    troublesome to detect all URLs to be requested.
    
    It could be more easy using phantomjs, wget or something.

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -2,56 +2,57 @@ require "sitespec"
 require "rack/jekyll"
 require "nokogiri"
 
-module Rack
-  class Interceptor # todo: a better name
-    def initialize(app)
-      @app = app
-      @cache = {}
-    end
+class Spider
+  def initialize(app)
+    @app = app
+    @cache = {}
+  end
 
-    def call(env)
-      path = env["PATH_INFO"]
-      warn "Rack::Interceptor is intercepting #{path}."
-      @cache[path] ||= @app.call(env).tap do |triplet|
-        warn "Rack::Interceptor is making a cache of '#{path}'."
+  def call(env)
+    path = env["PATH_INFO"]
 
-        # todo:
-        #   * Add server error handling as Sitespec::Builder#call does
-        #   * Give correct args to Sitespec::Request.new
-        request = Sitespec::Request.new(:get, path)
-        response = Sitespec::Response.new(*triplet)
-        Sitespec::Writer.write(request, response)
+    unless @cache[path]
+      @cache[path] = @app.call(env)
+      status, header, body = *@cache[path]
+
+      if status == 200 && header["Content-Type"] == "text/html"
+        extract_urls(body, path).each do |url|
+          Sitespec.get url unless @cache[url]
+        end
       end
     end
+
+    @cache[path]
+  end
+
+  def extract_urls(html, current_path)
+    html = Nokogiri::HTML.parse(html.first)
+    tag_attr = [["a", "href"], ["img", "src"], ["script", "src"], ["link", "href"]]
+
+    urls = tag_attr.flat_map do |(tag, attr)|
+      html.search(tag).map {|node| node[attr]}.compact
+    end
+
+    # FIXME: This removes all URLs with a host or a schema whether or not it points your rack app.
+    site_urls = urls.reject {|url| url.match(%r[^(\w+:|//)])}
+                    .map {|url| URI.join("http://example.org", current_path, url).path}
   end
 end
 
-Sitespec.configuration.application = Rack::Interceptor.new(Rack::Jekyll.new(baseurl: ""))
-
-Thread.new {
-  Rack::Server.start(app: Sitespec.configuration.application, Port: 9292)
-}
-
-def wget(path)
-  command = "wget 'http://0.0.0.0:9292#{path}' -O /dev/null --quiet -p -r -l 0"
-  system(command)
-end
+Sitespec.configuration.application = Spider.new(Rack::Jekyll.new(baseurl: ""))
 
 describe "This site" do
   include Sitespec
   it "provides the sitemap" do
-    # require 'pry';binding.pry
-    # exit
     get "/sitemap.xml"
 
     sitemap = Nokogiri::XML.parse(@response.body)
     locations = (sitemap / "loc").map(&:text)
-    # it provides every page in its sitemap
+
+    # it provides every page in its sitemap and resource used or referenced in pages
     locations.each do |location|
       path = URI.parse(location).path
-      path = "/index.html" if path == "/" # adhoc
-      wget path
-      # system("wget -p '#{location}'")
+      get path
     end
   end
 end
