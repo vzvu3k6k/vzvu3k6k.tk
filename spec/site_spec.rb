17841e457775bd37fd56c03632444a64fcdf0a16
vzvu3k6k <vzvu3k6k@gmail.com>
2013-11-21 01:03:43 +0900

    Add Sitespec.get_with_resources and remove Spider
    There is no need to implement this feature as a Rack middleware.

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -2,44 +2,35 @@ require "sitespec"
 require "rack/jekyll"
 require "nokogiri"
 
-class Spider
-  def initialize(app)
-    @app = app
-    @cache = {}
-  end
+module Sitespec
 
-  def call(env)
-    path = env["PATH_INFO"]
+  module_function
 
-    unless @cache[path]
-      @cache[path] = @app.call(env)
-      status, header, body = *@cache[path]
+  def get_with_resources(path, params = {}, env = {})
+    get(path, params, env)
 
-      if status == 200 && header["Content-Type"] == "text/html"
-        extract_urls(body, path).each do |url|
-          Sitespec.get url unless @cache[url]
-        end
+    if @response.status == 200 && @response.header["Content-Type"] == "text/html"
+      extract_urls(@response.body, path).each do |url|
+        get url
       end
     end
-
-    @cache[path]
   end
 
   def extract_urls(html, current_path)
-    html = Nokogiri::HTML.parse(html.first)
+    html = Nokogiri::HTML.parse(html)
     tag_attr = [["a", "href"], ["img", "src"], ["script", "src"], ["link", "href"]]
-
     urls = tag_attr.flat_map do |(tag, attr)|
       html.search(tag).map {|node| node[attr]}.compact
     end
 
     # FIXME: This removes all URLs with a host or a schema whether or not it points your rack app.
-    site_urls = urls.reject {|url| url.match(%r[^(\w+:|//)])}
-                    .map {|url| URI.join("http://example.org", current_path, url).path}
+    site_urls = urls.reject {|url| url.match(%r[^(\w+:|//)]) }
+                    .map {|url| URI.join("http://example.org", current_path, url).path }
   end
 end
 
-Sitespec.configuration.application = Spider.new(Rack::Jekyll.new(baseurl: ""))
+
+Sitespec.configuration.application = Rack::Jekyll.new(baseurl: "")
 
 describe "This site" do
   include Sitespec
@@ -52,7 +43,7 @@ describe "This site" do
     # it provides every page in its sitemap and resource used or referenced in pages
     locations.each do |location|
       path = URI.parse(location).path
-      get path
+      get_with_resources path
     end
   end
 end
